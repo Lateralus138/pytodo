0f4ab4681f769605ef4c584cc17cd05fc7cac40c
Ian Pride <faithnomoread@yahoo.com>
2019-11-04 05:30:45 -0600

    Fix for Windows path names

--- pytodo.py
+++ pytodo.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 import os, sys
 
+# FUNCTIONS
+def _check_create(fileOrDir,type):
+    """Create directory or file if it doesn't exist."""
+    def printerr():
+        print('Could not create ' 
+            + fileOrDir + '\nIt\'s ' 
+            + 'possible you do not have ' 
+            + 'the correct permissions.\n')
+    def printsuccess():
+        print(fileOrDir + ' was created successfully.\n')
+    def printattempt():
+        print('Attempting to create ' + fileOrDir)
+    if type.lower() == 'dir' or type.lower() == 'directory':
+        if not os.path.isdir(fileOrDir):
+                try:
+                    printattempt()
+                    os.mkdir(fileOrDir)
+                except:
+                    printerr()
+                    return
+                else:
+                    printsuccess()
+                    return True
+    if type.lower() == 'file':
+        if not os.path.isfile(fileOrDir):
+                try:
+                    printattempt()
+                    mkfile(fileOrDir)
+                    # os.mknod(fileOrDir)
+                except:
+                    printerr()
+                    return
+                else:
+                    printsuccess()
+                    return True
+
 def os_platform():
     platforms = {
         'linux1' : 'Linux',
@@ -11,6 +47,10 @@ def os_platform():
     if sys.platform not in platforms:
         return sys.platform
     return platforms[sys.platform]
+
+def mkfile(file):
+    with open(file, 'a'):
+        os.utime(file, None)
 
 # Enable logging
 import logging
@@ -41,46 +81,13 @@ else:
     HOME = os.environ['USERPROFILE']
 PYTODO_HOME = HOME + '/.pytodo'
 PYTODO_LIST = PYTODO_HOME + '/pytodo.todo'
+PYTODO_HOME = PYTODO_HOME.replace('\\','/')
+PYTODO_LIST = PYTODO_LIST.replace('\\','/')
 
 # GLOBALS
 log.debug('\nOperating System: ' + OS_TYPE 
     + '\nPython Home: ' + PYTODO_HOME 
     + '\nPython List: ' + PYTODO_LIST + '\n')
-
-# FUNCTIONS
-def _check_create(fileOrDir,type):
-    """Create directory or file if it doesn't exist."""
-    def printerr():
-        print('Could not create ' 
-            + fileOrDir + '\nIt\'s ' 
-            + 'possible you do not have ' 
-            + 'the correct permissions.\n')
-    def printsuccess():
-        print(fileOrDir + ' was created successfully.\n')
-    def printattempt():
-        print('Attempting to create ' + fileOrDir)
-    if type.lower() == 'dir' or type.lower() == 'directory':
-        if not os.path.isdir(fileOrDir):
-                try:
-                    printattempt()
-                    os.mkdir(fileOrDir)
-                except:
-                    printerr()
-                    return
-                else:
-                    printsuccess()
-                    return True
-    if type.lower() == 'file':
-        if not os.path.isfile(fileOrDir):
-                try:
-                    printattempt()
-                    os.mknod(fileOrDir)
-                except:
-                    printerr()
-                    return
-                else:
-                    printsuccess()
-                    return True
 
 ## Check for a good environment
 _check_create(PYTODO_HOME,'directory')
